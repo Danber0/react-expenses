24496efe49767e7731e23173bd73f0428ee82115
Danber <steem.sobol@mail.ru>
2023-09-08 18:14:03 -0400

    change url to mock backend

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // Configuration
-  baseURL: "https://853969987cc8476a.mokky.dev",
+  baseURL: "https://b5dea5ad014629c4.mokky.dev",
 });
